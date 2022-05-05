eff2f401ce3cd932783683623a69a5ff16a788d7
Rutbel Carlos <rttitoca@unsa.edu.pe>
2022-05-04 23:16:25 -0500

    se agrego metodo esBaseGabriel para verfificar si el numero esta en dicha base

--- src/main/java/com/example/project/Exercise1.java
+++ src/main/java/com/example/project/Exercise1.java
@@ -15,8 +15,35 @@ public class Exercise1 {
 	}
 
 	public String convertirBaseGabriel(int a) {
-
-		// TO DO
+		String strNum = Integer.toString(a);
+		char[] digitos = strNum.toCharArray();
+		
+		if(esBaseGabriel(a)){
+			return "esta en base gab";
+		}
 		return "El numero proporcionado no esta en base Gabriel.";
+	}
+	public boolean esBaseGabriel(int n){
+
+		String strNum = Integer.toString(n);
+		char[] digitos = strNum.toCharArray();
+		int suma = 0;
+		int cont = 0;
+		for(int i=digitos.length-1;i>=0;i--){
+			int d = Integer.parseInt(String.valueOf(digitos[i]));
+			if(d > 2){
+				return false;
+			}
+			if(d==2){
+				if(cont > 1)
+					return false;
+				cont++;
+				if(suma != 0)
+					return false;
+			}
+			suma += d;
+		}
+		return true;
+
 	}
 }
